89e27b5b3530b6257d617a59853eac18ce58c0c4
gopals <gopalsuthar@mirafra.com>
2023-12-05 12:24:57 +0530

    adding controller_agent_config_unit_test directory

---
+++ unit_test/hvl_unit_test/controller_unit_test/controller_agent_config_unit_test/.svunit.f
@@ -0,0 +1,10 @@
++incdir+/hwetools/work_area/frontend/gopal_b7/git_test1/i3c_project/i3c_avip/unit_test/hvl_unit_test/controller_unit_test/controller_agent_config_unit_test
++incdir+/hwetools/svunit/svunit_base/junit-xml
+/hwetools/svunit/svunit_base/junit-xml/junit_xml.sv
++incdir+/hwetools/svunit/svunit_base
+/hwetools/svunit/svunit_base/svunit_pkg.sv
+-f /hwetools/work_area/frontend/gopal_b7/git_test1/i3c_project/i3c_avip/unit_test/hvl_unit_test/controller_unit_test/controller_agent_config_unit_test/./svunit.f
+/hwetools/work_area/frontend/gopal_b7/git_test1/i3c_project/i3c_avip/unit_test/hvl_unit_test/controller_unit_test/controller_agent_config_unit_test/./i3c_controller_agent_config_unit_test.sv
++incdir+/hwetools/work_area/frontend/gopal_b7/git_test1/i3c_project/i3c_avip/unit_test/hvl_unit_test/controller_unit_test/controller_agent_config_unit_test/.
+.__testsuite.sv
+.testrunner.sv
