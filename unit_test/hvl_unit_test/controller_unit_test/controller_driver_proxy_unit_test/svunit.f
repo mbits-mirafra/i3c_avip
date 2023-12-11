2ca054c8ca9222db785f165db7d2623de37b21d9
muneebullashariff <muneebullashariff@gmail.com>
2023-12-11 16:04:32 +0530

    Added the unit_test for controller driver proxy using svmock

---
+++ unit_test/hvl_unit_test/controller_unit_test/controller_driver_proxy_unit_test/svunit.f
@@ -0,0 +1,6 @@
++incdir+$I3C_PROJECT/src/globals/
++incdir+$I3C_PROJECT/src/hvl_top/master/
++incdir+$SVUNIT_INSTALL/svunit_base/util
++incdir+$SVUNIT_INSTALL/svunit_base/uvm-mock/
++incdir+$SVMOCK_INSTALL/src
++incdir+$SVMOCK_INSTALL/src/uvm-mock
