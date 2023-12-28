296bdd4f5445d646ed3614ffcf57eee052095e9e
mahadevaswamy05 <mahadevaswamy@mirafra.com>
2023-12-29 00:27:30 +0530

    added controller_monitor_bfm and proxy code

--- src/hdl_top/controller_agent_bfm/i3c_controller_monitor_bfm.sv
+++ src/hdl_top/controller_agent_bfm/i3c_controller_monitor_bfm.sv
@@ -1,11 +1,8 @@
 `ifndef I3C_CONTROLLER_MONITOR_BFM_INCLUDED_
 `define I3C_CONTROLLER_MONITOR_BFM_INCLUDED_
 
-//-------------------------------------------------------
-//Module : i3c controller Monitor BFM
-//Description : connects the controller monitor bfm with the monitor proxy
-//-------------------------------------------------------
-import i3c_globals_pkg::*;
+import i3c_globals_pkg::*; 
+
 interface i3c_controller_monitor_bfm(input pclk, 
                                  input areset, 
                                  input scl_i,
@@ -18,26 +15,170 @@ interface i3c_controller_monitor_bfm(input pclk,
                                  //input sda
                                 );
  
- //-------------------------------------------------------
- // Package : Importing Uvm Pakckage and Test Package
- //-------------------------------------------------------
  import uvm_pkg::*;
- `include "uvm_macros.svh"
- 
- 
- //-------------------------------------------------------
- //Package : Importing I3C Global Package and I3C controller Package
- //-------------------------------------------------------
+`include "uvm_macros.svh" 
  import i3c_controller_pkg::*;
  import i3c_controller_pkg::i3c_controller_monitor_proxy;
  
-  //Variable : i3c_controller_mon_proxy_h
-  //Creating the handle for proxy driver
  i3c_controller_monitor_proxy i3c_controller_mon_proxy_h;
+ string name = "I3C_controller_MONITOR_BFM";
 
  initial begin
-   $display("i3c controller Monitor BFM");
+   $display(name);
  end
+
+ task wait_for_reset();
+    @(negedge areset);
+    @(posedge areset);
+ endtask: wait_for_reset
+task sample_idle_state();
+ @(posedge pclk);
+endtask: sample_idle_state
+
+task wait_for_idle_state();
+  @(posedge pclk);
+  while(scl_i!=1 && sda_i!=1) begin
+   @(posedge pclk);
+  end
+endtask: wait_for_idle_state
+
+task sample_data(inout i3c_transfer_bits_s struct_packet,inout i3c_transfer_cfg_s struct_cfg);
+detect_start();
+
+sample_target_address(struct_packet.targetAddress,struct_packet.targetAddressStatus);
+sample_operation(struct_packet.operation);
+sampleAddressAck(struct_packet.targetAddressStatus);
+  if(struct_packet.targetAddressStatus == ACK) begin
+   if(struct_packet.operation == WRITE) begin
+      sample_write_data(struct_packet.writeData[0]);
+      sampleWdataAck();
+      end else begin
+      sample_read_data(struct_packet.readData[0]);
+      sample_ack();
+      end
+  end else begin
+     detect_stop();
+  end
+     detect_stop();
+endtask: sample_data
+
+task detect_start();
+  bit [1:0] scl_local;
+  bit [1:0] sda_local;
+
+  do begin
+    @(negedge pclk);
+    scl_local = {scl_local[0], scl_i};
+    sda_local = {sda_local[0], sda_i};
+  end while(!(sda_local == NEGEDGE && scl_local == 2'b11) );
+endtask: detect_start
+
+task sample_target_address(output bit [6:0] address, output bit ack);
+
+  @(posedge pclk);
+//  state = ADDRESS;
+  for(int k=0;k < 7; k++) begin
+    detect_posedge_scl();
+    address[k] = sda_i;
+  end
+
+  if(address[6:0] != 7'b1010101) begin
+    ack = NACK;
+  end
+  else begin
+    ack = ACK;
+  end
+endtask: sample_target_address
+
+task sample_operation(output operationType_e wr_rd);
+  bit operation;
+  @(posedge pclk); 
+  detect_posedge_scl();
+  operation = sda_i;
+ if(operation == 0)
+   wr_rd = WRITE;
+ else
+   wr_rd = READ;
+endtask: sample_operation
+
+task sampleAddressAck(output bit ack);
+  @(posedge pclk); 
+  detect_negedge_scl();
+  ack = sda_i;
+  repeat(2)
+    @(posedge pclk); 
+endtask: sampleAddressAck
+
+task sample_write_data(output bit [7:0] wdata);
+    for(int k=0;k < DATA_WIDTH; k++) begin
+      detect_posedge_scl();
+      wdata[k] = sda_i;
+    end
+endtask: sample_write_data
+
+task sampleWdataAck();
+  @(posedge pclk); 
+  detect_negedge_scl();
+  repeat(2)
+   @(posedge pclk); 
+endtask: sampleWdataAck
+
+
+task sample_read_data(output bit[7:0] rdata);
+for(int k=0;k < DATA_WIDTH; k++) begin
+    detect_negedge_scl();
+    rdata[k] = sda_i;
+    detect_posedge_scl();
+  end
+endtask :sample_read_data
+
+task sample_ack();
+  detect_negedge_scl();
+  detect_posedge_scl();
+endtask :sample_ack
+
+task detect_stop();
+  bit [1:0] scl_local;
+  bit [1:0] sda_local;
+
+  do begin
+    @(negedge pclk);
+    scl_local = {scl_local[0], scl_i};
+    sda_local = {sda_local[0], sda_i};
+  end while(!(sda_local == POSEDGE && scl_local == 2'b11) );
+endtask: detect_stop
+
+
+task detect_posedge_scl();
+  bit [1:0] scl_local;
+  edge_detect_e scl_edge_value;
+
+  scl_local = 2'b11;
+  do begin
+    @(negedge pclk);
+    scl_local = {scl_local[0], scl_i};
+  end while(!(scl_local == POSEDGE));
+
+  scl_edge_value = edge_detect_e'(scl_local);
+
+endtask: detect_posedge_scl
+  
+task detect_negedge_scl();
+  bit [1:0] scl_local;
+  edge_detect_e scl_edge_value;
+
+  scl_local = 2'b11;
+  do begin
+    @(negedge pclk);
+    scl_local = {scl_local[0], scl_i};
+  end while(!(scl_local == NEGEDGE));
+
+  scl_edge_value = edge_detect_e'(scl_local);
+
+ endtask: detect_negedge_scl
+
+
+
 
 endinterface : i3c_controller_monitor_bfm
 
--- src/hvl_top/controller/i3c_controller_monitor_proxy.sv
+++ src/hvl_top/controller/i3c_controller_monitor_proxy.sv
@@ -1,19 +1,16 @@
 `ifndef I3C_CONTROLLER_MONITOR_PROXY_INCLUDED_
 `define I3C_CONTROLLER_MONITOR_PROXY_INCLUDED_
 
-//--------------------------------------------------------------------------------------------
-// Class: i3c_controller_monitor_proxy
-// <Description_here>
-//--------------------------------------------------------------------------------------------
 class i3c_controller_monitor_proxy extends uvm_component;
   `uvm_component_utils(i3c_controller_monitor_proxy)
 
+  i3c_controller_tx tx;
   i3c_controller_agent_config i3c_controller_agent_cfg_h;
 
+  virtual i3c_controller_monitor_bfm i3c_controller_mon_bfm_h;
+
   uvm_analysis_port #(i3c_controller_tx)controller_analysis_port;
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
+
   extern function new(string name = "i3c_controller_monitor_proxy", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual function void connect_phase(uvm_phase phase);
@@ -23,25 +20,12 @@ class i3c_controller_monitor_proxy extends uvm_component;
 
 endclass : i3c_controller_monitor_proxy
 
-//--------------------------------------------------------------------------------------------
-// Construct: new
-//
-// Parameters:
-//  name - i3c_controller_monitor_proxy
-//  parent - parent under which this component is created
-//--------------------------------------------------------------------------------------------
 function i3c_controller_monitor_proxy::new(string name = "i3c_controller_monitor_proxy",
-                                 uvm_component parent = null);
+                                                  uvm_component parent = null);
   super.new(name, parent);
+  controller_analysis_port = new("controller_analysis_port",this);
 endfunction : new
 
-//--------------------------------------------------------------------------------------------
-// Function: build_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 function void i3c_controller_monitor_proxy::build_phase(uvm_phase phase);
   super.build_phase(phase);
   if(!uvm_config_db #(i3c_controller_agent_config)::get(this,"","i3c_controller_agent_config",i3c_controller_agent_cfg_h))begin
@@ -50,57 +34,50 @@ function void i3c_controller_monitor_proxy::build_phase(uvm_phase phase);
     controller_analysis_port=new("controller_analysis_port",this);
 endfunction : build_phase
 
-//--------------------------------------------------------------------------------------------
-// Function: connect_phase
-// <Description_here>
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
-
 function void i3c_controller_monitor_proxy::connect_phase(uvm_phase phase);
   super.connect_phase(phase);
 endfunction : connect_phase
 
-//--------------------------------------------------------------------------------------------
-// Function: end_of_elaboration_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 function void i3c_controller_monitor_proxy::end_of_elaboration_phase(uvm_phase phase);
   super.end_of_elaboration_phase(phase);
+  i3c_controller_mon_bfm_h.i3c_controller_mon_proxy_h = this;
 endfunction  : end_of_elaboration_phase
 
-//--------------------------------------------------------------------------------------------
-// Function: start_of_simulation_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 function void i3c_controller_monitor_proxy::start_of_simulation_phase(uvm_phase phase);
   super.start_of_simulation_phase(phase);
 endfunction : start_of_simulation_phase
 
-//--------------------------------------------------------------------------------------------
-// Task: run_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 task i3c_controller_monitor_proxy::run_phase(uvm_phase phase);
+//  phase.raise_objection(this, "i3c_controller_monitor_proxy");
 
-  phase.raise_objection(this, "i3c_controller_monitor_proxy");
+//  super.run_phase(phase);
+  i3c_controller_tx tx_packet;
 
-  super.run_phase(phase);
+  tx_packet = i3c_controller_tx::type_id::create("tx_packet");
 
-  // Work here
-  // ...
+  `uvm_info(get_type_name(),"Running the Monitor Proxy", UVM_HIGH)
 
-  phase.drop_objection(this);
+  `uvm_info(get_type_name(), "Waiting for reset", UVM_HIGH);
+  i3c_controller_mon_bfm_h.wait_for_reset();
+  i3c_controller_mon_bfm_h.sample_idle_state();
+ 
+  forever begin
+   i3c_transfer_bits_s struct_packet;
+   i3c_transfer_cfg_s struct_cfg;
 
+   i3c_controller_mon_bfm_h.wait_for_idle_state();
+  
+   //i3c_controller_seq_item_converter::from_class(req, struct_packet);
+   i3c_controller_cfg_converter::from_class(i3c_controller_agent_cfg_h, struct_cfg);
+   i3c_controller_mon_bfm_h.sample_data(struct_packet,struct_cfg);
+   i3c_controller_seq_item_converter::to_class(struct_packet,tx_packet);
+   
+  `uvm_info(get_type_name(), "Reset detected", UVM_HIGH);
+  `uvm_info(get_type_name(), "Sampling Idle_state", UVM_HIGH);
+ 
+  `uvm_info(get_type_name(), "Sampled Idle_state", UVM_HIGH);
+   controller_analysis_port.write(tx_packet);
+ end
 endtask : run_phase
 
 `endif
