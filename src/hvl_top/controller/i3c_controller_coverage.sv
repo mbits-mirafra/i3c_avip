50e740587c043896daf11aed7573b577a180f66f
mahadevaswamy05 <mahadevaswamy@mirafra.com>
2023-12-30 22:01:43 +0530

    coverage code added for the controller

--- src/hvl_top/controller/i3c_controller_coverage.sv
+++ src/hvl_top/controller/i3c_controller_coverage.sv
@@ -2,20 +2,34 @@
 `define I3C_CONTROLLER_COVERAGE_INCLUDED_
 
 class i3c_controller_coverage extends uvm_subscriber#(i3c_controller_tx);
-  `uvm_component_utils(i3c_controller_coverage)
+ `uvm_component_utils(i3c_controller_coverage)
 
-    i3c_controller_agent_config i3c_controller_agent_cfg_h;
+ i3c_controller_agent_config i3c_controller_agent_cfg_h;
  
-  //-------------------------------------------------------
-  // Covergroup
-  // // TODO(mshariff): Add comments
-  // Covergroup consists of the various coverpoints based on the no. of the variables used to improve the coverage.
-  //-------------------------------------------------------
-  covergroup controller_covergroup with function sample (i3c_controller_agent_config cfg, i3c_controller_tx packet);
+covergroup controller_covergroup with function sample (i3c_controller_agent_config cfg, i3c_controller_tx packet);
     option.per_instance = 1;
+   
+   OPERATION_CP : coverpoint packet.operation{
+   option.comment = "Operation";
+   bins OPERATION = {0,1};
+   }
 
-    // Mode of the operation
-    
+   TARGET_ADRRESS_CP : coverpoint packet.targetAddress{
+   option.comment = "TargetAddress";
+   bins TARGETADDRESS = {[0:$]};
+   }
+
+   foreach(packet.writeData[i]) {
+   WRITEDATA_CP : coverpoint packet.writeData{
+   //option.comment = "writeData";
+   bins WRITEDATA = {[*]};
+   } 
+  }
+
+   READDATA_CP : coverpoint packet.readData{
+   option.comment = "readData";
+   bins READDATA = {[0:$]};
+   }
 
   endgroup : controller_covergroup
 
@@ -28,18 +42,10 @@ endclass : i3c_controller_coverage
 
 function i3c_controller_coverage::new(string name = "i3c_controller_coverage", uvm_component parent = null);
   super.new(name, parent);
-  // TODO(mshariff): Create the covergroup
-//`uvm_info(get_type_name(),$sformatf(controller_cg),UVM_LOW);
-//
-     controller_covergroup = new(); 
- // `uvm_info(get_type_name(),$sformatf(controller_cg),UVM_LOW); 
+ controller_covergroup = new(); 
+ //`uvm_info(get_type_name(),$sformatf(controller_cg),UVM_LOW); 
 endfunction : new
 
-//--------------------------------------------------------------------------------------------
-// Function: write
-// // TODO(mshariff): Add comments
-// sampiling is done
-//--------------------------------------------------------------------------------------------
 function void i3c_controller_coverage::write(i3c_controller_tx t);
 //  // TODO(mshariff): 
    controller_covergroup.sample(i3c_controller_agent_cfg_h,t);     
@@ -49,10 +55,6 @@ function void i3c_controller_coverage::write(i3c_controller_tx t);
 //
 endfunction: write
 
-//--------------------------------------------------------------------------------------------
-// Function: report_phase
-// Used for reporting the coverage instance percentage values
-//--------------------------------------------------------------------------------------------
 function void i3c_controller_coverage::report_phase(uvm_phase phase);
   `uvm_info(get_type_name(), $sformatf("controller Agent Coverage = %0.2f %%",controller_covergroup.get_coverage()), UVM_NONE);
   `uvm_info(get_type_name(), $sformatf("controller Agent Coverage") ,UVM_NONE);
