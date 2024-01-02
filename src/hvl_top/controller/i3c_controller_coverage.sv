317b6c00ee6e0343f4331ee45f731dc252e07ab3
mahadevaswamy05 <mahadevaswamy@mirafra.com>
2024-01-02 17:01:37 +0530

    added the coverage for the both controller and target

--- src/hvl_top/controller/i3c_controller_coverage.sv
+++ src/hvl_top/controller/i3c_controller_coverage.sv
@@ -4,9 +4,9 @@
 class i3c_controller_coverage extends uvm_subscriber#(i3c_controller_tx);
  `uvm_component_utils(i3c_controller_coverage)
 
- i3c_controller_agent_config i3c_controller_agent_cfg_h;
+i3c_controller_agent_config i3c_controller_agent_cfg_h;
  
-covergroup controller_covergroup with function sample (i3c_controller_agent_config cfg, i3c_controller_tx packet);
+covergroup i3c_controller_coverage with function sample (i3c_controller_agent_config cfg, i3c_controller_tx packet);
    option.per_instance = 1;
    
    OPERATION_CP : coverpoint packet.operation{
@@ -19,21 +19,32 @@ covergroup controller_covergroup with function sample (i3c_controller_agent_conf
    bins TARGETADDRESS = {[0:$]};
    }
 
-/*   foreach(packet.writeData[i]) begin
-   WRITEDATA_CP : coverpoint packet.writeData[0]{
-   option.comment = "writeData";
-   bins WRITEDATA = {[0:$]};
-   } 
-  end
+   TARGET_ADDRESS_STATUS_CP : coverpoint packet.targetAddressStatus{
+   option.comment = "targetAddressStatus";
+   bins TARGET_ADDRESS_STATUS = {0,1};
+   }
+/*
+   WRITEDATA_CP : coverpoint packet.writeData.size()*DATA_WIDTH {
+   option.comment = "Data size of the packet transfer";
+   bins W1 = {8};    
+  // bins WRITEDATA_WIDTH_2 = {16};
+  // bins WRITEDATA_WIDTH_3 = {24};
+  // bins WRITEDATA_WIDTH_4 = {32};
+  // bins WRITEDATA_WIDTH_5 = {64};
+  // bins WRITEDATA_WIDTH_6 = {[72:MAXIMUM_BITS]};
+ }
+
+ /*
    READDATA_CP : coverpoint packet.readData{
    option.comment = "readData";
-   bins READDATA = {[0:$]};
+master_coverage.sv   bins READDATA = {[0:$]};
    }
 
 */
-  endgroup : controller_covergroup
+  endgroup : i3c_controller_coverage
 
   extern function new(string name = "i3c_controller_coverage", uvm_component parent = null);
+  extern virtual function void display();
   extern virtual function void write(i3c_controller_tx t);
   extern virtual function void report_phase(uvm_phase phase);
 
@@ -42,22 +53,29 @@ endclass : i3c_controller_coverage
 
 function i3c_controller_coverage::new(string name = "i3c_controller_coverage", uvm_component parent = null);
   super.new(name, parent);
- controller_covergroup = new(); 
- //`uvm_info(get_type_name(),$sformatf(controller_cg),UVM_LOW); 
+  i3c_controller_coverage  = new(); 
 endfunction : new
+
+
+function void  i3c_controller_coverage::display();  
+  $display("");
+  $display("--------------------------------------");
+  $display(" COVERAGE");
+  $display("--------------------------------------");
+  $display("");
+endfunction : display
+
 
 function void i3c_controller_coverage::write(i3c_controller_tx t);
 //  // TODO(mshariff): 
-   controller_covergroup.sample(i3c_controller_agent_cfg_h,t);     
-  // `uvm_info(get_type_name(),$sformatf("controller_cg=%0d",controller_cg),UVM_LOW);
-//
- //  `uvm_info(get_type_name(),$sformatf(controller_cg),UVM_LOW);
-//
+  `uvm_info("MUNEEB_DEBUG", $sformatf("Config values = %0s",i3c_controller_agent_cfg_h.sprint()), UVM_HIGH);
+   i3c_controller_coverage.sample(i3c_controller_agent_cfg_h,t);     
 endfunction: write
 
 function void i3c_controller_coverage::report_phase(uvm_phase phase);
-  `uvm_info(get_type_name(), $sformatf("controller Agent Coverage = %0.2f %%",controller_covergroup.get_coverage()), UVM_NONE);
-  `uvm_info(get_type_name(), $sformatf("controller Agent Coverage") ,UVM_NONE);
+display(); 
+`uvm_info(get_type_name(), $sformatf("controller Agent Coverage = %0.2f %%",i3c_controller_coverage.get_coverage()), UVM_NONE);
+//  `uvm_info(get_type_name(), $sformatf("controller Agent Coverage") ,UVM_NONE);
 endfunction: report_phase
 `endif
 
--- src/hvl_top/target/i3c_target_coverage.sv
+++ src/hvl_top/target/i3c_target_coverage.sv
@@ -8,26 +8,24 @@ class i3c_target_coverage extends uvm_subscriber#(i3c_target_tx);
 
   i3c_target_agent_config i3c_target_agent_cfg_h;
 
-  //-------------------------------------------------------
-  // Covergroup
-  // Covergroup consists of the various coverpoints based on the no. of the variables used to improve the coverage.
-  //-------------------------------------------------------
-  covergroup target_covergroup with function sample (i3c_target_agent_config cfg, i3c_target_tx packet);
+ covergroup target_covergroup with function sample (i3c_target_agent_config cfg, i3c_target_tx packet);
   option.per_instance = 1;
+  
+     
+   OPERATION_CP : coverpoint packet.operation{
+   option.comment = "Operation";
+   bins OPERATION = {0,1};
+   }
 
-    // Mode of the operation
-    
-    //this coverpoint is to check the target address width
-    TARGET_ADDRESS_WID_CP : coverpoint cfg.targetAddress {
-      option.comment = "Width of the the target address";
+   TARGET_ADRRESS_CP : coverpoint packet.targetAddress{
+   option.comment = "TargetAddress";
+   bins TARGETADDRESS = {[0:$]};
+   }
 
-      bins TARGET_ADDRESS_WIDTH_7 = {7};
-      bins TARGET_ADDRESS_WIDTH_10 = {10};
-    }
 
-    //this coverpoint is to check the target_addr_ack 
-    //target_ADDR_ACK_BIT  : coverpoint packet.target_addr_ack {
-    //  option.comment = "target addr ack bit to dete";
+  /*
+  //target_ADDR_ACK_BIT  : coverpoint packet.target_addr_ack {
+  //  option.comment = "target addr ack bit to dete";
 
     //  bins target_ADDR_ACK = {1};
     //  bins target_ADDR_NACK = {0};
@@ -76,35 +74,22 @@ class i3c_target_coverage extends uvm_subscriber#(i3c_target_tx);
       bins LSB_FIRST = {0};
       bins MSB_FIRST = {1};
     }
-    
+    */
   endgroup :target_covergroup
 
 
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
-  extern function new(string name = "i3c_target_coverage", uvm_component parent = null);
+   extern function new(string name = "i3c_target_coverage", uvm_component parent = null);
   extern virtual function void display();
   extern virtual function void write(i3c_target_tx t);
   extern virtual function void report_phase(uvm_phase phase);
 
 endclass : i3c_target_coverage
 
-//--------------------------------------------------------------------------------------------
-// Construct: new
-//
-// Parameters:
-//  name - i3c_target_coverage
-//  parent - parent under which this component is created
-//--------------------------------------------------------------------------------------------
 function i3c_target_coverage::new(string name = "i3c_target_coverage", uvm_component parent = null);
   super.new(name, parent);
    target_covergroup = new(); 
 endfunction : new
 
-//--------------------------------------------------------------------------------------------
-// Contains the display statements 
-//--------------------------------------------------------------------------------------------
 function void i3c_target_coverage::display();
   $display("");
   $display("--------------------------------------");
@@ -112,23 +97,16 @@ function void i3c_target_coverage::display();
   $display("--------------------------------------");
   $display("");
 endfunction : display
-//--------------------------------------------------------------------------------------------
-// Function: write
-// To acess the subscriber write function is required with default parameter as t
-//--------------------------------------------------------------------------------------------
+
 function void i3c_target_coverage::write(i3c_target_tx t);
  `uvm_info("DEBUG_m_coverage", $sformatf("I3C_target_TX %0p",t),UVM_NONE);
 
     target_covergroup.sample(i3c_target_agent_cfg_h,t);     
 endfunction: write
 
-//--------------------------------------------------------------------------------------------
-// Function: report_phase
-// Used for reporting the coverage instance percentage values
-//--------------------------------------------------------------------------------------------
 function void i3c_target_coverage::report_phase(uvm_phase phase);
   display();
-  `uvm_info(get_type_name(), $sformatf("target Agent Coverage = %0.2f %%",
+ `uvm_info(get_type_name(), $sformatf("target Agent Coverage = %0.2f %%",
                                        target_covergroup.get_coverage()), UVM_NONE);
 endfunction: report_phase
 `endif
