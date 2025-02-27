2f5bbf27fe5690219282a74651d0bc548ea400dc
mahadevaswamy05 <mahadevaswamy@mirafra.com>
2025-02-27 11:02:48 +0530

    updated the testcases for solving the scoreboard error

--- src/hvl_top/test/i3c_writeOperationWith64bitsData_test.sv
+++ src/hvl_top/test/i3c_writeOperationWith64bitsData_test.sv
@@ -9,6 +9,7 @@ class i3c_writeOperationWith64bitsData_test extends i3c_base_test;
 
   extern function new(string name = "i3c_writeOperationWith64bitsData_test", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
+  extern virtual function void setup_env_cfg();
   extern virtual task run_phase(uvm_phase phase);
 
 endclass : i3c_writeOperationWith64bitsData_test
@@ -22,8 +23,12 @@ function void i3c_writeOperationWith64bitsData_test::build_phase(uvm_phase phase
   super.build_phase(phase);
 endfunction : build_phase
 
-task i3c_writeOperationWith64bitsData_test::run_phase(uvm_phase phase);
+function void i3c_writeOperationWith64bitsData_test::setup_env_cfg();
+  super.setup_env_cfg();
+  i3c_env_cfg_h.writeReadMode_h = ONLY_WRITE;
+endfunction: setup_env_cfg
 
+task i3c_writeOperationWith64bitsData_test::run_phase(uvm_phase phase);
 
   i3c_virtual_writeOperationWith64bitsData_seq_h = i3c_virtual_writeOperationWith64bitsData_seq::type_id::create("i3c_virtual_writeOperationWith64bitsData_seq_h");
 
