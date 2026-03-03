6dfa2592dbd7a61f482c1200fc822f2b9a2c1fb9
Sinchana B R <sinchanabr@feserver.mirafra.com>
2026-03-03 14:35:38 +0530

    pkg: update i3c package with new apb_env_config file, apb_master_pkg and top_virtual_sequencer

--- src/hvl_top/env/i3c_env_pkg.sv
+++ src/hvl_top/env/i3c_env_pkg.sv
@@ -1,3 +1,4 @@
+/*
 `ifndef I3C_ENV_PKG_INCLUDED_
 `define I3C_ENV_PKG_INCLUDED_
 
@@ -18,4 +19,32 @@ package i3c_env_pkg;
 
 endpackage : i3c_env_pkg
 
+`endif
+
+*/
+
+`ifndef I3C_ENV_PKG_INCLUDED_
+`define I3C_ENV_PKG_INCLUDED_
+ 
+package i3c_env_pkg;
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
+ 
+  import i3c_globals_pkg::*;
+ 
+//added apb_master pkg
+  import apb_master_pkg::*;
+  import i3c_target_pkg::*;
+ 
+  `include "i3c_env_config.sv"
+  `include "top_virtual_sequencer.sv"
+  `include "i3c_scoreboard.sv" 
+  `include "i3c_scoreboard_expactedTargetAddressNACK.sv"
+  `include "i3c_env.sv"
+ 
+  //added apb_config
+`include "apb_env_config.sv"
+ 
+endpackage : i3c_env_pkg
+ 
 `endif
